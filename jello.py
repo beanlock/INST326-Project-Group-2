0d59de905fe47ce5f9e88c2d447b71f63e80c8f5
DylanJameison <dylan.jameison08@gmail.com>
2024-03-28 22:20:10 -0400

    Update jello.py

--- jello.py
+++ jello.py
@@ -3,4 +3,11 @@ print("jello")
 pizza parkour dance
 (insert epic gaming music)
 '''
-"Hi"
+print("One Piece is peak fiction")
+int(input " What would you rank One Piece")
+OnePiece = 100
+if input <= OnePiece:
+    print("Stop the cap ")
+elif input >= OnePiece:
+    print("True")
+
