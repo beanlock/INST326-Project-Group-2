5dce978eddf76f9c2723a987dff717bbca138abd
beanlock <145821604+beanlock@users.noreply.github.com>
2024-03-28 22:38:33 -0400

    removed bad opinion
    script is now safe

--- jello.py
+++ jello.py
@@ -4,10 +4,3 @@ pizza parkour dance
 (insert epic gaming music)
 '''
 print("One Piece is peak fiction")
-int(input " What would you rank One Piece")
-OnePiece = 100
-if input <= OnePiece:
-    print("Stop the cap ")
-elif input >= OnePiece:
-    print("True")
-
